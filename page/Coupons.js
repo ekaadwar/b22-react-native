ed4acca8c702bb6bdec9862d59c50a50e83e5437
Eka Fajhari Adwar <eka.adwar@gmail.com>
2021-07-13 16:42:06 +0700

    Memindahkan program halaman Coupons dari App.js ke page page/Coupons.js

---
+++ page/Coupons.js
@@ -0,0 +1,126 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, TouchableOpacity, TextInput } from "react-native";
+// import { FaBeer } from "react-icons/fa";
+
+export default function App() {
+  const [text, onChangeText] = React.useState(null);
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.sectionHeader}>
+        <Text style={styles.headerContent}>My Coupons</Text>
+      </View>
+      <TextInput style={styles.searcing} onChangeText={onChangeText} placeholder="Browse coupons" value={text} />
+      <Text style={{ marginTop: 20 }}>Claim coupons by clicking it</Text>
+      <TouchableOpacity style={styles.cardYellow}>
+        <View style={styles.cardImg} />
+        <View style={styles.cardContent}>
+          <Text style={{ fontWeight: "bold" }}>Get a cup of coffee for free on sunday morning</Text>
+          <Text>Only at 7 to 9 AM</Text>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.cardGreen}>
+        <View style={styles.cardImg} />
+        <View style={styles.cardContent}>
+          <Text style={{ fontWeight: "bold" }}>HAPPY MOTHER’S DAY!</Text>
+          <Text>Get one of our favorite menu for free!</Text>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.cardRed}>
+        <View style={styles.cardImg} />
+        <View style={styles.cardContent}>
+          <Text style={{ fontWeight: "bold" }}>HAPPY HALLOWEEN!</Text>
+          <Text>Do you like chicken wings? Get 1 free only if you buy pinky promise</Text>
+        </View>
+      </TouchableOpacity>
+      <Text style={{ marginTop: 60 }}>You have no coupons left</Text>
+    </View>
+  );
+}
+
+const primaryButtonY = 50;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#ECECEC",
+    padding: 40,
+    alignItems: "center",
+  },
+
+  sectionHeader: {
+    alignItems: "center",
+  },
+
+  searcing: {
+    backgroundColor: "#fff",
+    paddingVertical: 7,
+    paddingHorizontal: 20,
+    borderRadius: 10,
+    width: "100%",
+  },
+
+  headerContent: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 55,
+  },
+
+  cardYellow: {
+    marginTop: 20,
+    backgroundColor: "#F5C361",
+    padding: 12,
+    borderRadius: 20,
+    width: "100%",
+    height: 100,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+
+  cardGreen: {
+    marginTop: 20,
+    backgroundColor: "#88B788",
+    padding: 12,
+    borderRadius: 20,
+    width: "100%",
+    height: 100,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+
+  cardRed: {
+    marginTop: 20,
+    backgroundColor: "#C59378",
+    padding: 12,
+    borderRadius: 20,
+    width: "100%",
+    height: 100,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+
+  cardImg: {
+    // backgroundColor: "orange",
+    flex: 1,
+  },
+
+  cardContent: {
+    // backgroundColor: "coral",
+    flex: 3,
+  },
+
+  buttonBrow: {
+    backgroundColor: "#6A4029",
+    alignItems: "center",
+    justifyContent: "center",
+    height: primaryButtonY,
+    borderRadius: primaryButtonY / 5,
+  },
+
+  textLightLg: {
+    color: "#fff",
+    fontWeight: "bold",
+    fontSize: 15,
+  },
+});
