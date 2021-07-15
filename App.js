fd8e94dc26747a84d3aa940e8295263c443960b9
Eka Fajhari Adwar <eka.adwar@gmail.com>
2021-07-15 09:40:38 +0700

    Memisahkan component Header dari file App.js

--- App.js
+++ App.js
@@ -4,14 +4,8 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
 import React, { Component } from "react";
-import {
-  StyleSheet,
-  Text,
-  Touchable,
-  TouchableOpacity,
-  View,
-} from "react-native";
 
+import Header from "./src/components/Header";
 import HomeScreen from "./src/screens/HomeScreen";
 import ProductDetails from "./src/screens/ProductDetails";
 import Cart from "./src/screens/Cart";
@@ -20,57 +14,7 @@ import Payment from "./src/screens/Payment";
 import FavoriteProducts from "./src/screens/FavoriteProducts";
 import PromoProducts from "./src/screens/PromoProducts";
 
-import Icon from "react-native-vector-icons/FontAwesome";
-import IconFeather from "react-native-vector-icons/Feather";
-import { bold } from "ansi-colors";
-
 const Stack = createStackNavigator();
-
-const Header = ({ navigation, scene }) => {
-  // console.log(scene.descriptor.options.title);
-  const title = scene.descriptor.options.title;
-  const page = scene.route.name;
-  // console.log(title);
-
-  return (
-    <View style={headerStyles.header}>
-      <TouchableOpacity onPress={() => navigation.goBack()}>
-        <Icon
-          name="chevron-left"
-          size={20}
-          color={page === "details" ? "#fff" : "#000"}
-        />
-      </TouchableOpacity>
-
-      {page !== "details" && <Text style={headerStyles.title}>{title}</Text>}
-
-      {page === "details" ? (
-        <TouchableOpacity onPress={() => navigation.navigate("cart")}>
-          <IconFeather name="shopping-cart" size={20} color="#000" />
-        </TouchableOpacity>
-      ) : (
-        <View />
-      )}
-    </View>
-  );
-};
-
-const headerStyles = StyleSheet.create({
-  header: {
-    height: 80,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    paddingTop: 40,
-    paddingHorizontal: 40,
-    // backgroundColor: "#362115",
-  },
-
-  title: {
-    fontWeight: "bold",
-    fontSize: 20,
-  },
-});
 
 export default class App extends Component {
   render() {
---
+++ src/components/Header.js
@@ -0,0 +1,54 @@
+import React from "react";
+
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+import Icon from "react-native-vector-icons/FontAwesome";
+import IconFeather from "react-native-vector-icons/Feather";
+
+const Header = ({ navigation, scene }) => {
+  // console.log(scene.descriptor.options.title);
+  const title = scene.descriptor.options.title;
+  const page = scene.route.name;
+  // console.log(title);
+
+  return (
+    <View style={headerStyles.header}>
+      <TouchableOpacity onPress={() => navigation.goBack()}>
+        <Icon
+          name="chevron-left"
+          size={20}
+          color={page === "details" ? "#fff" : "#000"}
+        />
+      </TouchableOpacity>
+
+      {page !== "details" && <Text style={headerStyles.title}>{title}</Text>}
+
+      {page === "details" ? (
+        <TouchableOpacity onPress={() => navigation.navigate("cart")}>
+          <IconFeather name="shopping-cart" size={20} color="#000" />
+        </TouchableOpacity>
+      ) : (
+        <View />
+      )}
+    </View>
+  );
+};
+
+const headerStyles = StyleSheet.create({
+  header: {
+    height: 80,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingTop: 40,
+    paddingHorizontal: 40,
+    // backgroundColor: "#362115",
+  },
+
+  title: {
+    fontWeight: "bold",
+    fontSize: 20,
+  },
+});
+
+export default Header;
